eb86898b06374b0c4464e033d88be2e89881ba63
Malanka Tharula <153574978+malankaT@users.noreply.github.com>
2024-08-12 20:38:34 +0530

    removed comments

--- fc110534-labsheet10/src/labsheets10/CreateTables.java
+++ fc110534-labsheet10/src/labsheets10/CreateTables.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 import java.sql.DriverManager; 
 import java.sql.SQLException;
 import java.sql.Statement;
-/*import any else if necessary */
 /**
  *
  * @author  Malanka Tharula
